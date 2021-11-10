e07750ab0fc3cdf74ea4dad818f588a73b7f6a81
mauricio muñoz corredor <arnoldmunoz@ustadistancia.edu.co>
2021-11-10 15:24:01 -0500

    video 113. creando la clase entity con @Entity

---
+++ src/main/java/com/bolsaideas/springbootjpa/app/models/entity/Cliente.java
@@ -0,0 +1,66 @@
+package com.bolsaideas.springbootjpa.app.models.entity;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+
+    private static final long serialVersionUID =1L;
+
+    @Id  //indica que es llave primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE) //solo se guarda la fecha
+    private Date createAt;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+}
