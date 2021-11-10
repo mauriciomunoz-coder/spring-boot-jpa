48d68ed60fbd1553151e80c81df3fc4026ca8c8b
mauricio muñoz corredor <arnoldmunoz@ustadistancia.edu.co>
2021-11-10 15:54:26 -0500

    video 117.  decorando la  vista listar con bootstrap

--- src/main/java/com/bolsaideas/springbootjpa/app/models/dao/ClienteDaoImpl.java
+++ src/main/java/com/bolsaideas/springbootjpa/app/models/dao/ClienteDaoImpl.java
@@ -1,12 +1,15 @@
 package com.bolsaideas.springbootjpa.app.models.dao;
 
 import com.bolsaideas.springbootjpa.app.models.entity.Cliente;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
+
+@Repository
 public class ClienteDaoImpl implements IClienteDao{
 
     @PersistenceContext
